68d4f875e87bfb12c987c32827d20b3d9e18b0e9
MajidJowDev <majid.jow@gmail.com>
2021-12-30 23:31:53 +0330

    initiation Commit

---
+++ src/main/java/com/testprojects/manytomanysample/ManyToManySampleApplication.java
@@ -0,0 +1,13 @@
+package com.testprojects.manytomanysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyToManySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testprojects/manytomanysample/ManyToManySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testprojects.manytomanysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
